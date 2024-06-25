3cc4656bde3896d0c099078c252927fc350b6667
Manogya Guragai <manogyaguragai24@gmail.com>
2024-06-25 17:34:07 +0545

    This is the first commit

---
+++ chatbot.py
@@ -0,0 +1,32 @@
+import os
+import google.generativeai as genai
+import dotenv
+from dotenv import load_dotenv
+import streamlit as st
+
+load_dotenv()
+
+genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
+
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+st.markdown("<h1 style='text-align: center; color: white;'>Gemini Powered ChatBot</h1>", unsafe_allow_html=True)
+st.markdown("<h6 style='text-align: center; color: white;'>Built By Manogya Guragai.</h1>", unsafe_allow_html=True)
+
+chat = model.start_chat(history=[])
+
+def answer(user_question):
+    response = chat.send_message(user_question)
+    return response
+
+quest = st.text_input("Ask a question:")
+btn = st.button("Ask", type="primary")
+
+if btn and quest:
+    result = answer(quest)
+    st.subheader("Response : ")
+    for word in result:
+        st.text(word.text)
+        
+
+
